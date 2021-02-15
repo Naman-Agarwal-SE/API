8bb0b457d316cae8d27a0c4ac3744af516e6ed6a
angularprac01 <namanagr1998@gmail.com>
2021-02-15 16:06:07 +0530

    redeploy

--- config/express.js
+++ config/express.js
@@ -3,6 +3,13 @@ const express=require('express');
 const routes = require('../server/routes');
 
 const app = express();
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api',routes);
